62fce22af2216475bac7e4a4ccd73842f970bc9b
DrWoodcodes <60747277+DrWoodcodes@users.noreply.github.com>
2024-03-13 01:59:15 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * This code opens up a simple GUI CODE. When the button is hit, the words "Music Unleashed" change to "Hello".
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new GUI();        
+    }
+
+}
